de55cf052e9d8d3407be46ddbc243aef478f1cb7
Viktor Demčák <viktorik170@gmail.com>
2024-01-31 03:46:47 +0100

    Questions filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,9 +30,9 @@ return [
 
     'disks' => [
 
-        'local' => [
+        'questions' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app') . '/questions',
             'throw' => false,
         ],
 
@@ -41,18 +41,6 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
-        ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
 
