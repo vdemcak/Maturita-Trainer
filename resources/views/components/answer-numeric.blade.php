de5f4308f838f0c545a0a0d8ace141139d038ca7
Viktor Demčák <viktorik170@gmail.com>
2024-02-03 00:28:58 +0100

    Fixed user being able to submit null as an answer

--- resources/views/components/answer-numeric.blade.php
+++ resources/views/components/answer-numeric.blade.php
@@ -7,7 +7,7 @@
 
     <button
         class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
-        wire:click="submitAnswer(answers['{{ $q }}'])">
+        wire:click="submitAnswer(answers['{{ $q }}'] ?? '')">
         Odoslať
     </button>
 </div>
