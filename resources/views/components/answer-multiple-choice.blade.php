02735b9e51f23ae6d73167b95a3d6cb2fc2a5962
Viktor Demčák <viktorik170@gmail.com>
2024-02-03 04:52:44 +0100

    Minor formatting fixes

--- resources/views/components/answer-multiple-choice.blade.php
+++ resources/views/components/answer-multiple-choice.blade.php
@@ -1,11 +1,10 @@
 @foreach($choices as $choice)
     <button
         wire:click="submitAnswer('{{ $loop->index }}')"
+
         class="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 disabled:opacity-50 disabled:hover:bg-white
-        @if($questions[$q]['is_correct'] === true && $questions[$q]['answer'] === $choice)
-            bg-green-200 text-green-600 border-green-400 hover:bg-green-200 hover:text-green-600
-        @endif
-        "
+        {{ $questions[$q]['is_correct'] === true && $questions[$q]['answer'] === $choice ? 'bg-green-200 text-green-600 border-green-400 hover:bg-green-200 hover:text-green-600' : '' }}"
+
         @if($questions[$q]['is_correct'] === true)
             disabled
         @endif
