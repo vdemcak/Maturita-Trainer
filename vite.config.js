22ac7db00506af23245835d5811102f91bfa52a1
Viktor Demčák <viktorik170@gmail.com>
2024-02-03 01:25:48 +0100

    Fixed minor build errors

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/main.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'node_modules/katex/dist/katex.css'],
             refresh: true,
         }),
     ],
